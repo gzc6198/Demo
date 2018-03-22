ad2e296bbb037733d4ca2e37db3d5af9d26ae956
gzc6198 <gongzc_python@163.com>
2018-03-22 17:36:05 +0800

    创建模型及url配置

--- Post/views.py
+++ Post/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+    return render(request,'post_list.html')
+def post_create(request):
+    return render(request,'create_post.html')
+def post_edit(request):
+    return render(request,'edit_post.html')
+def post_read(request):
+    return render(request,'read_post.html')
